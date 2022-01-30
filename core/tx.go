725f606cfc40db177e9be1ce06a52f72a1a68e61
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-01-30 13:12:19 +0530

    hash transactions

--- core/tx.go
+++ core/tx.go
@@ -1,7 +1,30 @@
 package core
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+)
+
 type Transaction struct {
 	ID      []byte
 	Inputs  []TXInput
 	Outputs []TXOutput
+}
+
+func (tx Transaction) Hash() ([]byte, error) {
+	serialData, err := tx.serialize()
+	if err != nil {
+		return []byte{}, err
+	}
+
+	hash := sha256.Sum256(serialData)
+	return hash[:], nil
+}
+
+func (tx Transaction) serialize() ([]byte, error) {
+	var buff bytes.Buffer
+	encoder := gob.NewEncoder(&buff)
+	err := encoder.Encode(tx)
+	return buff.Bytes(), err
 }
