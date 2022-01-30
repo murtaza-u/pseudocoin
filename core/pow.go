8c51eefcc63ffdc2de9e05a3e205fb5acd5f44c4
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-01-30 12:07:04 +0530

    Algorithm to calculate nonce using PoW consensus

--- core/pow.go
+++ core/pow.go
@@ -1,7 +1,12 @@
 package core
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"log"
+	"math"
 	"math/big"
+	"time"
 )
 
 type PoW struct {
@@ -9,10 +14,52 @@ type PoW struct {
 	Target *big.Int
 }
 
-const targetBits = 16
+const (
+	targetBits = 16
+	maxNonce   = math.MaxUint64
+)
 
 func NewPoW(b *Block) *PoW {
 	target := big.NewInt(1)
 	target = target.Lsh(target, 256-targetBits)
 	return &PoW{b, target}
+}
+
+func (pow *PoW) prepareData(nonce uint64) []byte {
+	var targetBytes, nonceBytes, timeBytes []byte
+
+	targetBytes, err := IntToBytes(targetBits)
+	nonceBytes, err = IntToBytes(int64(nonce))
+	timeBytes, err = IntToBytes(time.Now().Unix())
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return bytes.Join([][]byte{
+		targetBytes,
+		nonceBytes,
+		timeBytes,
+		pow.block.prevBlockHash,
+		pow.block.hashTXs(),
+	}, []byte{})
+}
+
+func (pow *PoW) Run() ([]byte, uint64) {
+	var hash [32]byte
+	var hashInt big.Int
+	var nonce uint64
+
+	for nonce < maxNonce {
+		hash = sha256.Sum256(pow.prepareData(nonce))
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.Target) == -1 {
+			break
+		}
+
+		nonce++
+	}
+
+	return hash[:], nonce
 }
