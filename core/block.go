a4b3136f84ea895bc3c04b85502892925c9abe43
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-03 22:59:15 +0530

    implemented hashTX method

--- core/block.go
+++ core/block.go
@@ -26,3 +26,18 @@ func DeserializeBlock(encData []byte) (Block, error) {
 	err := decoder.Decode(&block)
 	return block, err
 }
+
+func (b *Block) HashTXs() ([]byte, error) {
+	var txs [][]byte
+
+	for _, tx := range b.Transactions {
+		serialData, err := tx.serialize()
+		if err != nil {
+			return nil, err
+		}
+		txs = append(txs, serialData)
+	}
+
+	tree := NewMerkleTree(txs)
+	return tree.Root.Data, nil
+}
