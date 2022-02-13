1bf15edd52b793776c3710d3a7e39508b86a249d
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-13 09:45:53 +0530

    generate a new public-private keypair using the elliptic curve cryptograpy algorithm

--- core/wallet.go
+++ core/wallet.go
@@ -1,8 +1,37 @@
 package core
 
-import "crypto/ecdsa"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+)
 
 type Wallet struct {
 	PrivKey ecdsa.PrivateKey
 	PubKey  []byte
+}
+
+func newKeyPair() (ecdsa.PrivateKey, []byte, error) {
+	curve := elliptic.P256()
+	priv, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		return ecdsa.PrivateKey{}, nil, err
+	}
+
+	pub := append(priv.PublicKey.X.Bytes(), priv.PublicKey.Y.Bytes()...)
+	return *priv, pub, nil
+}
+
+func NewWallet() (Wallet, error) {
+	priv, pub, err := newKeyPair()
+	if err != nil {
+		return Wallet{}, err
+	}
+
+	wallet := Wallet{
+		PrivKey: priv,
+		PubKey:  pub,
+	}
+
+	return wallet, nil
 }
