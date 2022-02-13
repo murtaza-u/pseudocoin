3cc1fa916290c858bd46ecfed9bfdb399bff9efc
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-13 20:50:19 +0530

    check whether a transaction is initiated by a particular address

--- core/txinput.go
+++ core/txinput.go
@@ -1,8 +1,20 @@
 package core
 
+import "bytes"
+
 type TXInput struct {
 	TxID      []byte
 	Out       int
 	PublicKey []byte
 	Signature []byte
+}
+
+// check whether the address initiated the transaction
+func (in TXInput) UsesKey(pubKeyHash []byte) (bool, error) {
+	lockingHash, err := HashPubKey(in.PublicKey)
+	if err != nil {
+		return false, err
+	}
+
+	return bytes.Compare(lockingHash, pubKeyHash) == 0, nil
 }
