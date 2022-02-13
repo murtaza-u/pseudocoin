4730f82f2812342e0d35c0c331a6ab6fc4317e2f
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-13 20:37:48 +0530

    UTXO set method: Update() Updates the UTXO set, removing used Outputs and adding new ones, if any.

--- core/utxo_set.go
+++ core/utxo_set.go
@@ -50,3 +50,65 @@ func (u *UTXOSet) Reindex() error {
 
 	return err
 }
+
+func (u *UTXOSet) update(block Block) error {
+	db := u.Blockchain.DB
+
+	err := db.Update(func(t *bolt.Tx) error {
+		b := t.Bucket([]byte(utxoBucket))
+
+		for _, tx := range block.Transactions {
+			if !tx.IsCoinbase() {
+				for _, in := range tx.Inputs {
+					updatedOutputs := TXOutputs{}
+					outs, err := DeserializeOutputs(b.Get(in.TxID))
+					if err != nil {
+						return err
+					}
+
+					for outIDX, out := range outs.Outputs {
+						if outIDX == in.Out {
+							updatedOutputs.Outputs = append(updatedOutputs.Outputs, out)
+						}
+					}
+
+					if len(updatedOutputs.Outputs) == 0 {
+						err = b.Delete(in.TxID)
+						if err != nil {
+							return err
+						}
+					} else {
+						serial, err := updatedOutputs.Serialize()
+						if err != nil {
+							return err
+						}
+
+						err = b.Put(in.TxID, serial)
+						if err != nil {
+							return err
+						}
+					}
+				}
+			}
+
+			newOutputs := TXOutputs{}
+			for _, out := range tx.Outputs {
+				newOutputs.Outputs = append(newOutputs.Outputs, out)
+			}
+
+			serial, err := newOutputs.Serialize()
+			if err != nil {
+				return err
+			}
+
+			err = b.Put(tx.ID, serial)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
+	return err
+}
