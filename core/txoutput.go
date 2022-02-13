8774585edb33a78836bd300e90cad039814e3a06
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-13 11:32:56 +0530

    sign the output with public key hash

--- core/txoutput.go
+++ core/txoutput.go
@@ -1,6 +1,20 @@
 package core
 
+import "github.com/mr-tron/base58"
+
 type TXOutput struct {
 	Value      uint
 	PubkeyHash []byte
+}
+
+// sign the output
+func (out *TXOutput) Lock(address string) error {
+	pubKeyHash, err := base58.Decode(address)
+	if err != nil {
+		return err
+	}
+
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-addressChecksumLen]
+	out.PubkeyHash = pubKeyHash
+	return nil
 }
