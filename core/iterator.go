584869763699fed1e72766890722b1e66fc053f1
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-13 19:13:08 +0530

    get the next block using blockchain iterator

--- core/iterator.go
+++ core/iterator.go
@@ -6,3 +6,23 @@ type Iterator struct {
 	currentBlockHash []byte
 	db               *bolt.DB
 }
+
+func (i *Iterator) Next() (*Block, error) {
+	if len(i.currentBlockHash) == 0 {
+		return nil, nil
+	}
+
+	var block Block
+
+	err := i.db.View(func(t *bolt.Tx) error {
+		b := t.Bucket([]byte(blocksBucket))
+		data := b.Get(i.currentBlockHash)
+
+		var err error
+		block, err = DeserializeBlock(data)
+
+		return err
+	})
+
+	return &block, err
+}
