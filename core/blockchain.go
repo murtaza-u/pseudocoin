e50b5e03d046d822d224a94dcd50fefe712c48d6
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-13 15:38:14 +0530

    create a new blockchain

---
+++ core/blockchain.go
@@ -0,0 +1,68 @@
+package core
+
+import (
+	"errors"
+
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbFile              = "blockchain.db"
+	blocksBucket        = "blocks"
+	genesisCoinbaseData = "I use Arch BTW"
+)
+
+type Blockchain struct {
+	Tip []byte
+	DB  *bolt.DB
+}
+
+func CreateBlockchain(address string) (Blockchain, error) {
+	if DBExists(dbFile) {
+		return Blockchain{}, errors.New("Blockchain already exists")
+	}
+
+	cbtx, err := NewCBTX(address, genesisCoinbaseData)
+	if err != nil {
+		return Blockchain{}, err
+	}
+
+	genesis, err := NewGenesisBlock(cbtx)
+	if err != nil {
+		return Blockchain{}, err
+	}
+
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		return Blockchain{}, err
+	}
+
+	err = db.Update(func(t *bolt.Tx) error {
+		b, err := t.CreateBucket([]byte(blocksBucket))
+		if err != nil {
+			return err
+		}
+
+		serial, err := genesis.Serialize()
+		if err != nil {
+			return err
+		}
+
+		err = b.Put(genesis.Hash, serial)
+		if err != nil {
+			return err
+		}
+
+		err = b.Put([]byte("l"), genesis.Hash)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return Blockchain{
+		Tip: genesis.Hash,
+		DB:  db,
+	}, err
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/murtaza-udaipurwala/core
 go 1.17
 
 require (
+	github.com/boltdb/bolt v1.3.1
 	github.com/mr-tron/base58 v1.2.0
 	golang.org/x/crypto v0.0.0-20220210151621-f4118a5b28e2
 )
+
+require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
