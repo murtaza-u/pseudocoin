32f14bc26b993ddb464a5ac864c665fa96e3a383
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-13 11:14:43 +0530

    help function to check if database already exists

--- core/utils.go
+++ core/utils.go
@@ -3,10 +3,19 @@ package core
 import (
 	"bytes"
 	"encoding/binary"
+	"os"
 )
 
 func IntToBytes(num int64) ([]byte, error) {
 	buff := new(bytes.Buffer)
 	err := binary.Write(buff, binary.BigEndian, num)
 	return buff.Bytes(), err
+}
+
+func DBExists(dbFile string) bool {
+	if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
 }
