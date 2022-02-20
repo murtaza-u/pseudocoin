5fe12ec72a587ee9d08733d1e5853f0deccc315d
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-21 02:12:26 +0530

    instantiate CLI

--- cli/cli.go
+++ cli/cli.go
@@ -6,3 +6,10 @@ type CLI struct {
 	Blockchain core.Blockchain
 	UTXOSet    core.UTXOSet
 }
+
+func NewCLI(bc core.Blockchain, UTXOSet core.UTXOSet) CLI {
+	return CLI{
+		Blockchain: bc,
+		UTXOSet:    UTXOSet,
+	}
+}
