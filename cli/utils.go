1e37a8e919f4177831a8f17e7961504b1b43204a
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-21 21:35:21 +0530

    function to generate a random string of lenght 6

--- cli/utils.go
+++ cli/utils.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"crypto/rand"
+	"io"
 	"io/ioutil"
 	"os"
 	"path"
@@ -36,4 +38,25 @@ func write(file string, data []byte) error {
 	}
 
 	return ioutil.WriteFile(path.Join(home, file), data, 0600)
+}
+
+var chars = []byte{
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
+	'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	'u', 'v', 'w', 'x', 'y', 'z',
+}
+
+func randomString() (string, error) {
+	b := make([]byte, 6)
+	_, err := io.ReadAtLeast(rand.Reader, b, 6)
+
+	if err != nil {
+		return "", err
+	}
+
+	for i := 0; i < len(b); i++ {
+		b[i] = chars[int(b[i])%len(chars)]
+	}
+
+	return string(b), nil
 }
