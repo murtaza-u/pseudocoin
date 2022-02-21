b193fc8f0e9b6d1b62b449e0e19e879d3e6eed33
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-21 22:52:25 +0530

    CLI.GetAddress

---
+++ cli/address.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"io/ioutil"
+
+	"github.com/murtaza-udaipurwala/pseudocoin/core"
+)
+
+type address struct {
+	Address string `json:"address"`
+}
+
+func (cli *CLI) GetAddress(arg string) (interface{}, error) {
+	var pubkey []byte
+	var err error
+
+	if fileExists(arg) {
+		pubkey, err = ioutil.ReadFile(arg)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		pubkey = []byte(arg)
+	}
+
+	w := core.Wallet{}
+	if err = w.DecodePubKeys(string(pubkey)); err != nil {
+		return nil, err
+	}
+
+	addr, err := w.GetAddress()
+	if err != nil {
+		return nil, err
+	}
+
+	return address{
+		Address: addr,
+	}, nil
+}
