6d029a41a7cb0984ecfa3f95094315929271f895
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-21 22:00:28 +0530

    simplify main.go. Printing result now handled by cli package

--- main.go
+++ main.go
@@ -1,27 +1,8 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-
-	"github.com/murtaza-udaipurwala/pseudocoin/cli"
-)
+import "github.com/murtaza-udaipurwala/pseudocoin/cli"
 
 func main() {
 	cli := cli.NewCLI()
-
-	res, err := cli.Run()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	b, err := json.MarshalIndent(res, "", "    ")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	fmt.Println(string(b))
+	cli.Print(cli.Run())
 }
