33df6e51cdf0c97ce9a56e68b494a26fe0d30e1f
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2022-02-22 13:49:14 +0530

    clean up: use CLI.rpcCall

--- cli/balance.go
+++ cli/balance.go
@@ -1,11 +1,8 @@
 package cli
 
 import (
-	"bytes"
 	"errors"
-	"net/http"
 
-	"github.com/gorilla/rpc/json"
 	"github.com/murtaza-udaipurwala/pseudocoin/core"
 )
 
@@ -23,25 +20,9 @@ func (cli *CLI) getBalance(addr string) (interface{}, error) {
 		return nil, errors.New("Invalid address")
 	}
 
-	msg, err := json.EncodeClientRequest("RPC.GetBalance", &balanceParams{Address: addr})
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", URL, bytes.NewBuffer(msg))
-	req.Header.Set("Content-Type", "application/json")
-
-	client := new(http.Client)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-
 	var balance balance
-	err = json.DecodeClientResponse(resp.Body, &balance)
+
+	err := cli.rpcCall("RPC.GetBalance", &balanceParams{Address: addr}, &balance)
 	if err != nil {
 		return nil, err
 	}
